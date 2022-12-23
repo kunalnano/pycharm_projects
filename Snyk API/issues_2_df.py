97944b2bc49fd392597f9b84aab8011819630cc5
Kunal Sharma <kunalnano@gmail.com>
2022-12-22 18:35:28 -0600

    This script defines a normalize_json function that takes the API response data and a project ID as input, and extracts and assigns values for each field in the output data. If a field is not present in the API response, the function assigns a default value of "N/A" to the field. The main function then calls this function to normalize the data for each project, and prints the resulting data as a table.

---
+++ Snyk API/issues_2_df.py
@@ -0,0 +1,77 @@
+import requests
+import pandas as pd
+from tabulate import tabulate
+
+
+def normalize_json(json_data, project_id):
+    normalized_data = []
+    for issue in json_data["data"]:
+        try:
+            description = issue["description"]
+        except KeyError:
+            description = "N/A"  # default value if "description" is not present
+        try:
+            severity = issue["severity"]
+        except KeyError:
+            severity = "N/A"  # default value if "severity" is not present
+        try:
+            package = issue["package"]["name"]
+        except KeyError:
+            package = "N/A"  # default value if "package" is not present
+        normalized_data.append({
+            "projectID": project_id,
+            "issueID": issue["id"],
+            "description": description,
+            "severity": severity,
+            "package": package,
+        })
+    return normalized_data
+
+
+def main():
+    TOKEN = "ffc32183-089f-4820-b009-02f845103670"
+    ORG_ID = "47f8d945-2af9-4b09-b8d9-6afe4f6e288a"
+
+    # Retrieve a list of projects within the organization
+    projects_url = f"https://api.snyk.io/rest/orgs/{ORG_ID}/projects?version=2022-04-06~experimental"
+    projects_response = requests.get(projects_url, headers={
+        "Content-Type": "application/json",
+        "Authorization": f"token {TOKEN}"
+    })
+    if projects_response.status_code != 200:
+        print(f"Error retrieving list of projects: {projects_response.text}")
+        exit()
+
+    projects_data = projects_response.json()
+    try:
+        project_ids = [p["id"] for p in projects_data["data"]]
+    except KeyError:
+        print(f"Error: 'data' not found in response: {projects_data}")
+        exit()
+
+    # Retrieve a list of issues for each project
+    issues = []
+    for project_id in project_ids:
+        issues_url = f"https://api.snyk.io/rest/orgs/{ORG_ID}/issues?&project_id={project_id}&version=2022-04-06~experimental"
+
+        issues_response = requests.get(issues_url, headers={
+            "Content-Type": "application/json",
+            "Authorization": f"token {TOKEN}"
+        })
+        if issues_response.status_code != 200:
+            print(f"Error retrieving list of issues for project {project_id}: {issues_response.text}")
+            continue
+
+        issues_data = issues_response.json()
+        normalized_data = normalize_json(issues_data, project_id)
+        issues.extend(normalized_data)
+
+    # Convert the list of issues to a Pandas DataFrame
+    df = pd.DataFrame(issues)
+
+    # Print the DataFrame as a table
+    print(tabulate(df, headers="keys", tablefmt="psql"))
+
+
+if __name__ == "__main__":
+    main()
